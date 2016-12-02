bd896f1dbe727c32d25a468de5bf2ae6bc21a6eb
xuyong <xuyong@lianxi.com>
2016-12-02 15:54:13 +0800

    pro
    pro

---
+++ XYYUIKit/XYYUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYYUIKit
+//
+//  Created by xuyong on 16/12/2.
+//  Copyright © 2016年 xuyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
