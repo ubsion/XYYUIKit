823a4ce6bc539e93aecfc2dc70d748e08df97e62
xuyong <xuyong@lianxi.com>
2016-12-02 15:55:21 +0800

    项目
    项目新建

---
+++ XYYUIKit/XYYUIKit/Classes/XYYUIKit.h
@@ -0,0 +1,16 @@
+//
+//  XYYUIKit.h
+//  XYYUIKit
+//
+//  Created by xuyong on 16/12/2.
+//  Copyright © 2016年 xuyong. All rights reserved.
+//  赋值封装常用控件属性，方便使用
+
+#ifndef XYYUIKit_h
+#define XYYUIKit_h
+
+#import "UILabel+XYYLabelKit.h"
+#import "UIImageView+XYYImageViewKit.h"
+#import "UIButton+XYYButtonKit.h"
+
+#endif /* XYYUIKit_h */
--- XYYUIKit/XYYUIKit/ViewController.m
+++ XYYUIKit/XYYUIKit/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "XYYUIKit.h"
+
 @interface ViewController ()
 
 @end
@@ -16,9 +18,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UILabel *myLabel = [UILabel labelTextColor:[UIColor redColor] fontSize:20];
+    myLabel.frame = CGRectMake(100, 100, 200, 44);
+    myLabel.text = @"中国共产党万岁!!!";
+    [self.view addSubview:myLabel];
+    
+    
+    UIButton *myButton = [UIButton buttonWithText:@"按钮点击" textColor:[UIColor blueColor] textFontSize:15 action:@selector(buttonClick:) target:self];
+    myButton.frame = CGRectMake(100, 200, 160, 44);
+    [self.view addSubview:myButton];
+    
+    UIButton *newButton = [UIButton buttonWithImageName:@"ui" action:@selector(buttonClickNew:) target:self];
+    newButton.frame = CGRectMake(100, 300, 160, 44);
+    [self.view addSubview:newButton];
 }
 
+
+-(void)buttonClick:(UIButton *)button
+{
+    NSLog(@"buttonClick");
+}
+
+
+-(void)buttonClickNew:(UIButton *)button
+{
+    NSLog(@"buttonNewClick");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
